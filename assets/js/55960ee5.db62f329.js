c5eba16b0e3e56104ac7f4511ac24182733eec0a
IwannabeRealnerD <IwannabeRealnerD@gmail.com>
2022-08-17 21:01:20 +0900

    Deploy website - based on 59244217a0b7d8fd604d0abd61140df183feb4d6

--- assets/js/55960ee5.db62f329.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[121],{8070:e=>{e.exports=JSON.parse('[{"label":"img","permalink":"/docs/tags/img","count":2},{"label":"alt","permalink":"/docs/tags/alt","count":2},{"label":"role","permalink":"/docs/tags/role","count":2},{"label":"A11Y","permalink":"/docs/tags/a-11-y","count":1}]')}}]);
